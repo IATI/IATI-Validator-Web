de360e2ae14049057935ede735ce0f9846294dd6
Bas Wanders <bas@wanderssoftware.com>
2018-03-07 13:34:24 +0100

    Message "Searching..." added when loading/searching organisations

--- src/app/organisations/organisations/organisations.component.ts
+++ src/app/organisations/organisations/organisations.component.ts
@@ -12,6 +12,7 @@ import { Observable } from 'rxjs/Observable';
 export class OrganisationsComponent implements OnInit {
   organisations: Organisation[] = [];
   filteredOrganisations: Organisation[] = [];
+  isSearching = false;
 
   constructor(private organisationService: OrganisationsService) { }
 
@@ -20,11 +21,12 @@ export class OrganisationsComponent implements OnInit {
   }
 
   searchOrganisation(name: string) {
+    this.isSearching = true;
     if (this.organisations === undefined || this.organisations.length === 0) {
       // get organisations from the web api
       this.organisationService.getOrganisations()
         .subscribe(org => this.organisations = org,
-          error => console.log(error),
+          error => {console.log(error); this.isSearching = false; },
           () => {
             // finished fetching organisations from web api, filter the organisations by name
             this.filterOrganisations(name);
@@ -52,5 +54,6 @@ export class OrganisationsComponent implements OnInit {
                                         return (a.name || '').toString().toLowerCase().localeCompare((b.name || '').toString().toLowerCase()) ;
                                       });
     }
+    this.isSearching = false;
   }
 }
