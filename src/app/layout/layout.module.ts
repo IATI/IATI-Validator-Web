1a3d264ff9c9ed3ec4d31dc476a3acf175efc18e
Bas Wanders <bas@wanderssoftware.com>
2018-03-12 17:19:38 +0100

    Hambermenu opens now on click

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    NgbModule,
   ],
   exports: [
     HeaderComponent,
