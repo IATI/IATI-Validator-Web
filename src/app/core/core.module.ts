eec8e1dcf75a17193cefedbfe8a3d0cb027eedaf
Bas Wanders <bas@wanderssoftware.com>
2018-03-13 18:01:40 +0100

    Load component and service added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,16 +3,28 @@ import { CommonModule } from '@angular/common';
 import { throwIfAlreadyLoaded } from './module-import-guard';
 import { HttpModule } from '@angular/http';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LogService } from './logging/log.service';
 import { LogPublishersService } from './logging/log-publishers.service';
+import { LoaderComponent } from './loader/loader.component';
+import { LoaderService } from './loader/loader.service';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpModule,
+    NgbModule,
   ],
-  declarations: [],
-  providers: [ LogService, LogPublishersService],
+  exports: [
+    LoaderComponent
+  ],
+  declarations: [
+    LoaderComponent
+  ],
+  providers: [
+    LogService,
+    LogPublishersService,
+    LoaderService],
 })
 export class CoreModule {
   constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
