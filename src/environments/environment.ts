4fc3b147dd1cbcb4288a5583a0d355e8973010da
Rolf Kleef <rolf@data4development.nl>
2019-09-05 21:49:52 +0200

    Clean up environments
    baseUrl and apiDataworkBench are done via env.js (injected in container on kubernetes)
    bucketnameSuffix no longer needed (via new generic API endpoint)
    sandbox and staging no longer needed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,5 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  baseUrl: 'https://9239879d-e1c3-4ad6-846a-fd8925283028.mock.pstmn.io',
   production: false,
-  apiDataworkBench: 'https://9239879d-e1c3-4ad6-846a-fd8925283028.mock.pstmn.io/api',
-  bucketnameSuffix: '',
-  // apiDataworkBench: 'http://dev1.dataworkbench.io/api',
-  // apiDataworkBench: 'http://localhost:3000/api',
 };
