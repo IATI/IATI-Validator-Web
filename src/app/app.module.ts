c6f0edd4ebd9554dd20127a632f1ab14a7179cf1
Edwin P. Magezi <edwinm_p@yahoo.com>
2021-01-28 08:29:26 +0300

    Simplify app.module.ts for future updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LocationStrategy, PathLocationStrategy } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
+// import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
-import { SharedModule } from './shared/shared.module';
-import { HomeComponent } from './home/home.component';
-import { LayoutModule } from './layout/layout.module';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AboutComponent } from './views/data-quality-feedback/about/about.component';
+// import { SharedModule } from './shared/shared.module';
+// import { HomeComponent } from './home/home.component';
+// import { LayoutModule } from './layout/layout.module';
+// import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+// import { AboutComponent } from './views/data-quality-feedback/about/about.component';
 import { GtagModule } from 'angular-gtag';
-import { OrganisationService } from './organisation/shared/organisation.service';
+// import { OrganisationService } from './organisation/shared/organisation.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent,
-    PageNotFoundComponent,
+    // HomeComponent,
+    // AboutComponent,
+    // PageNotFoundComponent,
   ],
   imports: [
     GtagModule.forRoot({ trackingId: 'UA-110230511-9', trackPageviews: true }),
     BrowserModule,
-    NgbModule.forRoot(),
-    HttpClientModule,
-    CoreModule,
-    SharedModule,
-    LayoutModule,
+    // NgbModule.forRoot(),
+    // HttpClientModule,
+    // CoreModule,
+    // SharedModule,
+    // LayoutModule,
     AppRoutingModule,
   ],
   providers: [
-    OrganisationService
+    // OrganisationService
   ],
   exports: [
-    CoreModule,
+    // CoreModule,
   ],
   bootstrap: [AppComponent]
 })
