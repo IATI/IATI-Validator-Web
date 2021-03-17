1f07b2a5e48bbee61535e603180846de1b201db2
nosvalds <niko@devinit.org>
2021-03-17 15:42:15 +0000

    env file setup for CD

---
+++ src/env.dev.js
@@ -0,0 +1,16 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  // API urls
+  window.__env.baseUrl = "http://stage.iativalidator.iatistandard.org";
+  window.__env.apiDataworkBench =
+    "http://stage.iativalidator.iatistandard.org/api/v1";
+
+  // staging password
+  window.__env.stagePass = "reallybigsecret";
+
+  // version display
+  // window.__env.valActualVersion = "1.6.2";
+  // window.__env.valServerVersion = "1.0.1";
+  // window.__env.valWebVersion = "1.4.0";
+})(this);
