726c7306202fcd5fdf1f18d61a7d2f9837999a90
nosvalds <niko@devinit.org>
2021-08-11 15:05:54 +0100

    add production deployment on Release workflow

--- src/env.prod.js
+++ src/env.prod.js
@@ -1,15 +1,23 @@
 (function (window) {
   window.__env = window.__env || {};
 
-  // API url
-  window.__env.baseUrl = "https://iativalidator.iatistandard.org";
-
+  // API urls
+  window.__env.baseUrl = "http://stage.iativalidator.iatistandard.org";
   window.__env.apiDataworkBench =
-    "https://iativalidator.iatistandard.org/api/v1";
+    "http://stage.iativalidator.iatistandard.org/api/v1";
 
-  window.__env.stagePass = null;
+  // window.__env.validatorServicesUrl = "http://localhost:7071/api"
+  // window.__env.validatorServicesUrl = "https://dev-api.iatistandard.org/vs"
+  window.__env.validatorServicesUrl = "https://api.iatistandard.org/vs"
 
-  window.__env.valActualVersion = "1.6.2";
-  window.__env.valServerVersion = "1.0.1";
-  window.__env.valWebVersion = "1.4.0";
+  // staging password
+  window.__env.stagePass = "reallybigsecret";
+
+  // guidance link baseUrl
+  window.__env.guidanceLinkBaseUrl = 'https://iatistandard.org/en/iati-standard'
+  
+  // version display
+  // window.__env.valActualVersion = "1.6.2";
+  // window.__env.valServerVersion = "1.0.1";
+  // window.__env.valWebVersion = "1.4.0";
 })(this);
