1ed6d2e85521971afe988d0c6ebfbe7de6fda397
nosvalds <niko@devinit.org>
2021-08-17 13:46:18 -0400

    get validator services version from API

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from './../../../environments/environment';
+import { VersionService } from '../shared/version.service';
+import { LogService } from './../../core/logging/log.service';
 
 @Component({
   selector: 'app-footer',
@@ -8,14 +10,23 @@ import { environment } from './../../../environments/environment';
 })
 export class FooterComponent implements OnInit {
   webVersion = '';
-  servicesVersion!: '?';
+  servicesVersion = '';
   apiVersion!: '?';
-  constructor() { }
+  constructor(private versionService: VersionService, private logger: LogService) { }
 
   ngOnInit() {
     this.webVersion = environment.version;
-    this.servicesVersion = window.__env.valServicesVersion;
+
+    this.versionService.getServicesVersion().subscribe(data => {
+      console.log(data);
+      this.servicesVersion = data;
+    },
+    error => this.logger.error(error),
+     () => {
+       if (this.servicesVersion === undefined) {
+         this.servicesVersion = 'X.X.X';
+       }
+     });
     this.apiVersion = window.__env.valApiVersion;
   }
-
 }
