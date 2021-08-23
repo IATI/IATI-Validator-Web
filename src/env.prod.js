ca5b549cfe684b2e157a6a21a0ab1c1d0b36e9c9
nosvalds <niko@devinit.org>
2021-08-23 11:22:41 -0400

    add api key for validator services

--- src/env.prod.js
+++ src/env.prod.js
@@ -2,12 +2,10 @@
   window.__env = window.__env || {};
 
   // API urls
-  window.__env.baseUrl = "https://dev-validator.iatistandard.org/";
-
-  // window.__env.validatorServicesUrl = "http://localhost:7071/api"
-  // window.__env.validatorServicesUrl = "https://dev-api.iatistandard.org/vs"
+  window.__env.baseUrl = "https://prod-validator.iatistandard.org/";
   window.__env.validatorServicesUrl = "https://api.iatistandard.org/vs"
 
+  // validator services api key
   window.__env.validatorServicesKeyName = 'Ocp-Apim-Subscription-Key';
   window.__env.validatorServicesKeyValue = '280182cc8cb8436cb94102f485034fd1';
 
@@ -17,8 +15,4 @@
   // guidance link baseUrl
   window.__env.guidanceLinkBaseUrl = 'https://iatistandard.org/en/iati-standard'
   
-  // version display
-  window.__env.valApiVersion = "2.0.0";
-  window.__env.valServicesVersion = "2.0.0";
-  window.__env.valWebVersion = "2.0.0";
 })(this);
