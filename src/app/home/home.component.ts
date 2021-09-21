afd6e72754903e5c82dea5d04d3ff4d300860444
Alex Lydiate <alexl@devinit.org>
2021-09-21 14:31:36 +0100

    Removed the password thing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,29 +11,6 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   async ngOnInit() {
-    // todo - this, properly - we've no sensitive stuff on the staging site,
-    // so this currently is just a quick "hey, you know this is the staging site, right?" check
-
-    if (window.__env.stagePass === undefined) {
-      return;
-    }
-
-    console.log(document.cookie);
-
-    if (document.cookie.includes('whynot')) {
-      return;
-    }
-
-    let password = null;
-
-    const getPass = async () => {
-      password = window.prompt('Please enter the passphrase to enter the staging site.', '');
-    };
-
-    while (password !== window.__env.stagePass) {
-      await getPass();
-    }
-
-    document.cookie = 'pass=whynot';
+    return;
   }
 }
