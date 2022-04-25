9fa7c1ee8b490ae1f02a0c81f2bd2ade9281129c
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-04-25 15:48:31 +0300

    Add extra report info component

---
+++ src/app/views/data-quality-feedback/extra-report-info/extra-report-info.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Document } from 'src/app/shared/document';
+import { LogService } from '../../../core/logging/log.service';
+
+@Component({
+  selector: 'app-extra-report-info',
+  templateUrl: './extra-report-info.component.html',
+  styleUrls: ['./extra-report-info.component.scss'],
+})
+export class ExtraReportInfoComponent implements OnInit {
+  @Input() document: Document = {} as any;
+
+  constructor(private logger: LogService) {}
+
+  ngOnInit() {}
+
+  fileName(): string {
+    if (this.document) {
+      return this.document.url;
+    }
+
+    return '';
+  }
+}
